4dfa2169a67b49ca336aa6ee0602e7fc5b63a5da
Alex <janouser11@gmail.com>
2017-02-15 21:33:15 -0500

    Adding old project to github

---
+++ IncomeEstimator/IncomeEstimator/Calculate.cs
@@ -0,0 +1,75 @@
+﻿namespace IncomeEstimator
+
+{
+  
+    internal class Calculate
+    {
+
+        public int hourlyWage, weeklyHours, wageForWeek, wageForMonth;
+        int wageForYear = 0;
+        public int[] arrayOfWages = new int[3];
+
+        public Calculate()
+        {
+        }
+
+        public Calculate(int wage, int hours)
+        {
+             hourlyWage = wage;
+             weeklyHours = hours;
+
+             arrayOfWages[0] = calculatePayForYear(hourlyWage, weeklyHours);
+             arrayOfWages[1] = calculatePayForMonth(hourlyWage, weeklyHours);
+             arrayOfWages[2] = calculatePayForWeek(hourlyWage, weeklyHours);
+        }
+
+
+
+        public int calculatePayForYear(int wage, int hours)
+        {
+            if (hours > 40)
+            {
+                int difference = hours - 40;
+                int overTime = (((wage / 2) + wage)) * difference;
+                this.wageForYear = ((wage * 40 * 52) + overTime * 52);
+            }
+            else
+            {
+                this.wageForYear = wage * hours * 52;
+            }
+
+            return wageForYear;
+        }
+
+        public int calculatePayForMonth(int wage, int hours)
+        {
+            if (hours > 40)
+            {
+                int difference = hours - 40;
+                int overTime = (((wage / 2) + wage)) * difference;
+                this.wageForMonth = ((wage * 40 * 4) + overTime *4);
+            } else
+            {
+                this.wageForMonth = wage * hours * 4;
+            }
+
+            return wageForMonth;
+        }
+
+
+        public int calculatePayForWeek(int wage, int hours)
+        {
+            if (hours > 40)
+            {
+                int difference = hours - 40;
+                int overTime = (((wage / 2) + wage)) * difference;
+                this.wageForWeek = ((wage * 40) + overTime);
+            } else
+            {
+                this.wageForWeek = wage * hours;
+            }    
+
+            return wageForWeek;
+        }
+    }  
+}
---
+++ IncomeEstimator/IncomeEstimator/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IncomeEstimator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();        
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            try {
+
+                int hourlyWage = int.Parse(textBox1.Text);
+                int weeklyHours = int.Parse(textBox2.Text);
+
+                Calculate calculate = new Calculate(hourlyWage, weeklyHours);
+
+                calculatedBlock.Text = "Yearly Pay: $" + calculate.arrayOfWages[0] + ".00" + "\nMonthly Pay: $" + calculate.arrayOfWages[1] + ".00" + "\nWeekly Pay: $" + calculate.arrayOfWages[2] +  ".00";
+            }
+
+            catch (Exception err)
+            {
+                MyWipedText.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
